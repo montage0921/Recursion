2c73eb61dff7cefec206d91775223d14001912ba
montage0921 <garyshi0921@gmail.com>
2023-03-19 13:30:54 -0400

    Finished

--- assignment-1.js
+++ assignment-1.js
@@ -3,6 +3,7 @@
 function fibs(n) {
   const arr = [];
 
+  if (n == 0) return arr;
   for (let i = 0; i <= n - 1; i++) {
     if (i === 0) arr.push(0);
     else if (i === 1) arr.push(1);
@@ -12,4 +13,19 @@ function fibs(n) {
   return arr;
 }
 
-console.log(fibs(8));
+//2. Recursive
+
+function fibsRec(n) {
+  if (n === 0) return [];
+  //base of recursion
+  else if (n === 1) return [0];
+  else if (n === 2) return [0, 1];
+  else {
+    const arr = fibsRec(n - 1);
+    const firstN = arr[arr.length - 2];
+    const secondN = arr[arr.length - 1];
+    const newN = firstN + secondN;
+    arr.push(newN);
+    return arr;
+  }
+}
